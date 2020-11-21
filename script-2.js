ff2edf6b906d272ef18877a59386af5f330e632d
Olga Uzun <67106495+ol4enok20101@users.noreply.github.com>
2020-11-21 17:18:21 +0200

    Add files via upload

---
+++ script-2.js
@@ -0,0 +1,25 @@
+// В HTML есть пустой список ul#ingredients.
+
+// В JS есть массив строк.
+
+// Напиши скрипт, который для каждого элемента массива ingredients создаст отдельный li, после чего вставит все li за одну операцию в список ul.ingredients. Для создания DOM - узлов используй document.createElement().
+
+const ingredients = [
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
+];
+
+const newArr = ingredients.map((value) => { 
+    const liItem = document.createElement('li');
+    liItem.textContent = value;
+    return liItem;
+})
+
+// console.log(...newArr)
+
+const list = document.querySelector('#ingredients');
+list.append(...newArr)
